606516e4f354f67ef6e3b446a80c871a181ea55d
laday <laday@crimson.ua.edu>
2016-10-10 19:21:16 -0500

    Still not working(-:

--- app/src/main/java/com/example/lday15/estimotetesting/MyApplication.java
+++ app/src/main/java/com/example/lday15/estimotetesting/MyApplication.java
@@ -1,9 +1,20 @@
 package com.example.lday15.estimotetesting;
 
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+
+import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
+import com.estimote.sdk.connection.internal.protocols.Operation;
 
+
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -21,6 +32,7 @@ public class MyApplication extends Application {
 
         beaconManager = new BeaconManager(getApplicationContext());
 
+
         beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
             @Override
             public void onServiceReady() {
@@ -28,6 +40,43 @@ public class MyApplication extends Application {
                         "monitored region",
                         UUID.fromString("/*UUID FROM OUR BEACON*/"),
                         /*MAJOR*/22504, /*MINOR*/48827));
+            }
+
+        });
+
+    }
+
+    public void showNotification(String title, String message) {
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[]{notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT);
+        Operation.Notification notification = (Operation.Notification) new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, (Notification) notification);
+
+
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> list) {
+                showNotification(
+                        "Your gate closes in 47 minutes.",
+                        "Current security wait time is 15 minutes, "
+                                + "and it's a 5 minute walk from security to the gate. "
+                                + "Looks like you've got plenty of time!");
+            }
+
+            @Override
+            public void onExitedRegion(Region region) {
+                // could add an "exit" notification too if you want (-:
             }
         });
     }
